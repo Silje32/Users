a53047233c4875147435e5196c4ac8e57a703138
Silje32 <msh070425@gmail.com>
2025-10-07 10:16:48 +0200

    Added my array of objects

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,6 +1,13 @@
 import { useState } from "react";
 
 export default function Users() {
+  const mockData = [
+    { username: "Ola Normann", email: "ola.normann@norge.no" },
+    { username: "Torleif", email: "torleif@kodehode.no" },
+    { username: "Jan Egil", email: "jan.egil@kodehode.no" },
+    { username: "Sander", email: "sander@kodehode.no" },
+  ];
+
   const [name, setName] = useState("");
   const [email, setEmail] = useState(null);
 
