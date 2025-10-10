c3a38e50da59f6e172845def4ceda472b99a6eb2
Silje32 <msh070425@gmail.com>
2025-10-10 14:59:14 +0200

    Added classNames in my User function and styled it

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import styles from "./Users.module.css";
 
 export default function Users() {
   const mockData = [
@@ -15,15 +16,15 @@ export default function Users() {
   const [newUser, setNewUser] = useState({});
 
   return (
-    <div>
-      <h1>USERS</h1>
+    <div className={styles.container}>
+      <h1 className={styles.h1}>USERS</h1>
       {data.map((user, index) => (
-        <div key={index}>
+        <div className={styles.userlist} key={index}>
           <h2>Navn: {user.username}</h2>
           <p>Epost: {user.email}</p>
         </div>
       ))}
-      <div>
+      <div className={styles.new_user}>
         <h3>Would you like to add a user to the list?</h3>
       </div>
 
